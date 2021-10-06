652136593deef3ea4583dcb6f3873dc6765790b6
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-06 03:06:26 +0100

    Created tasks route with dummy data.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/tasks', function (Request $request) {
+    return [
+        [
+            "id" => 1,
+            "details" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam et excepturi vitae dolor assumenda",
+            "category" => [
+                "name" => "Management",
+                "color" => "red"
+            ]
+        ],
+        [
+            "id" => 2,
+            "details" => "perferendis a modi quod iusto quo repellat repudiandae tenetur commodi dicta doloribus inventore sunt",
+            "category" => [
+                "name" => "Sales",
+                "color" => "yellow"
+            ]
+        ],
+    ];
+});
