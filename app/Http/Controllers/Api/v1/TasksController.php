cb228048e1e5f42de7ae388ef6e7d97380675849
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-07 09:55:08 +0100

    Added index method.

--- app/Http/Controllers/Api/v1/TasksController.php
+++ app/Http/Controllers/Api/v1/TasksController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
-    //
+    public function index()
+    {
+        return Task::with(["taskCategory"])->get();
+    }
 }
