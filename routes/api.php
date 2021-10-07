bc89927b3e46d57f44a55a8fbf0dbba236f77bb3
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-07 09:53:31 +0100

    Added task categories to route.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\TaskCategoriesController;
 use App\Http\Controllers\Api\v1\TasksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,25 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('task', TasksController::class);
-
-Route::middleware(['cors'])->get('/tasks', function (Request $request) {
-    return [
-        [
-            "id" => 1,
-            "details" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam et excepturi vitae dolor assumenda",
-            "category" => [
-                "name" => "Management",
-                "color" => "red"
-            ]
-        ],
-        [
-            "id" => 2,
-            "details" => "perferendis a modi quod iusto quo repellat repudiandae tenetur commodi dicta doloribus inventore sunt",
-            "category" => [
-                "name" => "Sales",
-                "color" => "yellow"
-            ]
-        ],
-    ];
-});
+Route::resource('tasks', TasksController::class);
+Route::resource('task-categories', TaskCategoriesController::class);
