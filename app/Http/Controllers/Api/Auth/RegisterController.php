62001a6c442d696e19a9f7c5151f84d33716ed59
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-07 18:04:26 +0100

    Created register controller.

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken("app_token")->plainTextToken;
+
+        return response(['user' => $user, 'token' => $token], 201);
+    }
+}
