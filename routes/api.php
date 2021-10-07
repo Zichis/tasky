6190676d28af752ef3d70f0e791a78c81476723a
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-07 18:05:38 +0100

    Added auth routes. Grouped routes that need authenticated user.

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,26 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\v1\TaskCategoriesController;
 use App\Http\Controllers\Api\v1\TasksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('tasks', TasksController::class);
-Route::resource('task-categories', TaskCategoriesController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('tasks', TasksController::class);
+    Route::resource('task-categories', TaskCategoriesController::class);
+    Route::delete('logout', function (Request $request) {
+        $request->user()->tokens()->delete();
+
+        return response(["message" => "Logged Out"], 204);
+    });
+});
+
+// Auth
+Route::post('/register', [RegisterController::class, 'store'])->name('user.register');
+Route::post('/login', [LoginController::class, 'login'])->name('user.login');
