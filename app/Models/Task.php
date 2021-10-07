e6b0c40a8b4f70669a682ef01adf02b5ddd8c79a
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-07 09:54:52 +0100

    Added fillable and relationship to task model.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["details", "task_category_id"];
+
+    // Relationship
+    /**
+     * Get the category that owns the Task
+     *
+     * @return BelongsTo
+     */
+    public function taskCategory(): BelongsTo
+    {
+        return $this->belongsTo(TaskCategory::class);
+    }
 }
