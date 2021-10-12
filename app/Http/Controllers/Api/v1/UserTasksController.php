27a91d2273855ff9c4e7d88aef6af40fcb14db0e
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-12 21:19:38 +0100

    Added store method.

--- app/Http/Controllers/Api/v1/UserTasksController.php
+++ app/Http/Controllers/Api/v1/UserTasksController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Task;
+use App\Models\TaskCategory;
 use Illuminate\Http\Request;
 
 class UserTasksController extends Controller
@@ -12,6 +13,32 @@ class UserTasksController extends Controller
     {
         $user = $request->user();
 
-        return Task::where('user_id', $user->id)->with(["taskCategory"])->get();
+        return Task::where('user_id', $user->id)->with(["taskCategory"])->orderBy('created_at', 'DESC')->get();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:50',
+            'details' => 'required',
+            'category' => 'required',
+            // TODO: Add color here. This depends on if category is a new category.
+            // Color should be red, blue, yellow or green
+        ]);
+
+        $category = TaskCategory::firstOrCreate(
+            ['name' => $request->get("category")["name"]],
+            ['color' => $request->get("color")]
+        );
+
+        Task::create([
+            "title" => $request->get("title"),
+            "details" => $request->get("details"),
+            "task_category_id" => $category->id,
+            "user_id" => $request->user()->id,
+            "createdby_id" => $request->user()->id
+        ]);
+
+        return response(["message" => "Success"], 200);
     }
 }
