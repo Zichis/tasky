58be2e9b4aefba8fa87e83c4d4abc31464c860b1
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-16 20:48:53 +0100

    Added relationship to model.

--- app/Models/TaskCategory.php
+++ app/Models/TaskCategory.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TaskCategory extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["name", "color"];
+    protected $fillable = ["name", "color", "user_id"];
 
     /**
      * Get all of the tasks for the TaskCategory
@@ -20,5 +21,15 @@ class TaskCategory extends Model
     public function tasks(): HasMany
     {
         return $this->hasMany(Task::class);
+    }
+
+    /**
+     * Get the user that owns the TaskCategory
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
