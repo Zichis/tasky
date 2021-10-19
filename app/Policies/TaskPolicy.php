8af4fc191082cc91715cf045ccc5f355ddeae43e
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-10-19 11:40:34 +0100

    Fixed phpstan erros in files.

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -33,12 +33,6 @@ class TaskPolicy
         return $user->id === $task->user_id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function create(User $user)
     {
         //
@@ -56,37 +50,16 @@ class TaskPolicy
         return $user->id === $task->user_id;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function delete(User $user, Task $task)
     {
         //
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function restore(User $user, Task $task)
     {
         //
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function forceDelete(User $user, Task $task)
     {
         //
