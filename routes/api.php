6ee3bff82defd169de4e699784d9abbb32c9a522
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-11-04 20:50:13 +0100

    Added task statuses to route.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\v1\TaskCategoriesController;
+use App\Http\Controllers\Api\v1\TaskStatusController;
 use App\Http\Controllers\Api\v1\UserTasksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks', UserTasksController::class);
     Route::get('task-categories/names', [TaskCategoriesController::class, "names"]);
     Route::apiResource('task-categories', TaskCategoriesController::class);
+    Route::apiResource('task-statuses', TaskStatusController::class);
     Route::post('logout', function (Request $request) {
         $request->user()->tokens()->delete();
 
