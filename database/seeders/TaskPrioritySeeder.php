bf76e8c75c636c3f25ad56582bd521ec7636c69f
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-01-28 11:44:37 +0100

    Created task priority seeder.

---
+++ database/seeders/TaskPrioritySeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TaskPriority;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TaskPrioritySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        TaskPriority::firstOrCreate([
+            'name' => 'Low',
+            'slug' => Str::slug('Low'),
+            'color' => '#e6e6e6'
+        ]);
+
+        TaskPriority::firstOrCreate([
+            'name' => 'Normal',
+            'slug' => Str::slug('Normal'),
+            'color' => '#80d4ff'
+        ]);
+
+        TaskPriority::firstOrCreate([
+            'name' => 'High',
+            'slug' => Str::slug('High'),
+            'color' => '#ffc266'
+        ]);
+
+        TaskPriority::firstOrCreate([
+            'name' => 'Urgent',
+            'slug' => Str::slug('Urgent'),
+            'color' => '#ff471a'
+        ]);
+    }
+}
