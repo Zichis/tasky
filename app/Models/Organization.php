bc2a0547b699e3769dc2ffb922e0e08e9dfe4d8e
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-02-07 12:38:57 +0100

    Changed routekey to slug.

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Str;
 
 class Organization extends Model
 {
@@ -21,6 +22,16 @@ class Organization extends Model
     public function superAdmin(): HasOne
     {
         return $this->hasOne(User::class);
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 
     /**
