54ad9ad7f2fba7bc7f538996a48c91126a72cf09
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-02-07 12:40:23 +0100

    Created organization seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TaskPrioritySeeder::class,
             TaskStatusSeeder::class,
-            UserSeeder::class
+            UserSeeder::class,
+            OrganizationSeeder::class
         ]);
     }
 }
